0bb94a3dbe8d548ed4f1c94242fa42192e446dd4
zxl <zhaoxl@foxmail.com>
2021-03-04 15:49:55 +0800

    run mmdet

--- det2/experiments/test_truck.sh
+++
@@ -1,6 +0,0 @@
-cd ../tools
-tag_name=1121
-which_model=../tools/output-1/model_0010999.pth
-var_out=../tools/vis
-cd ../demo  #../datasets/eval/baiguang/*
-python demo.py  --output $var_out  --config-file ../configs/Truck-Detection/faster_rcnn_R_50_FPN_1x.yaml  --input ../datasets/truck/truck_train_imgs/*     --opts MODEL.WEIGHTS $which_model  #../pretrained/output/model_0004999.pth
--- det2/experiments/val_truck.sh
+++
@@ -1,3 +0,0 @@
-var_weight=/data01/zxl/Truck-Detection/outputs/faster_rcnn_R_50_FPN_3x/model.pth
-cd ../tools
-python ./plain_train_net.py  --eval-only --num-gpus 1 --config-file ../configs/Truck-Detection/faster_rcnn_R_50_FPN_3x.yaml MODEL.WEIGHTS $var_weight
